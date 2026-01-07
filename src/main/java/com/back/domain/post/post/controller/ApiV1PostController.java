047b7a312c0e8c4b0b56e6554fa87eb624503e99
zunobk <junho4560@naver.com>
2026-01-08 02:14:17 +0900

    비밀번호까지 입력받아서 사칭을 못하게 막았다.

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -78,9 +78,12 @@ public class ApiV1PostController {
     @Operation(summary = "작성")
     public RsData<PostDto> write(
             @Valid @RequestBody PostWriteReqBody reqBody,
-            @NotBlank @Size(min = 2, max = 30) String username
+            @NotBlank @Size(min = 2, max = 30) String username,
+            @NotBlank @Size(min = 2, max = 30) String password
     ) {
         Member actor = memberService.findByUsername(username).get();
+        if ( !actor.getPassword().equals(password) ) throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+
         Post post = postService.write(actor, reqBody.title, reqBody.content);
 
         return new RsData<>(
