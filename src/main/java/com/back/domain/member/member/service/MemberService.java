1976cec1972a145055c21c40d85da5e287e48bfd
zunobk <junho4560@naver.com>
2026-01-11 22:55:42 +0900

    이제 글 작성시 username/password가 아니라 apiKey로 인증

--- src/main/java/com/back/domain/member/member/service/MemberService.java
+++ src/main/java/com/back/domain/member/member/service/MemberService.java
@@ -25,4 +25,8 @@ public class MemberService {
     public Optional<Member> findByUsername(String username) {
         return memberRepository.findByUsername(username);
     }
+
+    public Optional<Member> findByApiKey(String apiKey) {
+        return memberRepository.findByApiKey(apiKey);
+    }
 }
